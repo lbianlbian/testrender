24763096811915eeb9d75803bbd9c810c7776fae
lbianlbian <83378530+lbianlbian@users.noreply.github.com>
2026-08-08 15:12:58 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/", methods=["GET", "POST"])
+def request_router():
+    if request.method == "POST":
+        # handle POST logic
+        data = request.form.get("some_field")  # or request.json for JSON body
+        return "Handled POST"
+    else:
+        # pass the verification
+        return request.args.get("hub.challenge")
